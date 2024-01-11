831944f71a20120591362dabc7bde6792e681268
florenciamontini <flor_montini@hotmail.com>
2024-01-10 22:44:22 -0300

    validación nota distinta a null y distinta a cero - ngIf

--- src/app/components/cargar-notas/cargar-notas.component.ts
+++ src/app/components/cargar-notas/cargar-notas.component.ts
@@ -6,9 +6,12 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
   templateUrl: './cargar-notas.component.html',
   styleUrls: ['./cargar-notas.component.css']
 })
-export class CargarNotasComponent {
-  constructor(private formbuilder: FormBuilder) { }
 
+export class CargarNotasComponent {
+  esNotaVacia: boolean = false; // Variable para rastrear si 'nota' está vacío
+
+  constructor(private formbuilder: FormBuilder) { }
+  
   formularioCargaNotas = this.formbuilder.group({
     materia: ['', Validators.required],
     alumno: ['', Validators.required],
@@ -17,7 +20,15 @@ export class CargarNotasComponent {
 
   mostrarDatosFormulario(): void {
     console.log(this.formularioCargaNotas.value);
+    // Verificar si 'nota' está vacío y actualizar la variable esNotaVacia
+    const notaControl = this.formularioCargaNotas.get('nota') as FormControl;
+    this.esNotaVacia = !notaControl.value && notaControl.value == 0; //le agregue una validacion para q tmp pueda ser igual a 0
+
+  } 
+    //aca adentro de la funcion tengo que evaluar si nota es vacía, y si esta vacia setear como verd o falso otra variable 
+    //luego en html con ng if evaluar esa variable y mostrar la alerta segun corresponda
   }
 
+
   
-}
+
--- src/app/components/ver-notas/ver-notas.component.ts
+++ src/app/components/ver-notas/ver-notas.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-ver-notas',
   templateUrl: './ver-notas.component.html',
   styleUrls: ['./ver-notas.component.css']
 })
+
 export class VerNotasComponent {
 
+  materia = new FormControl('');
+  mostrarDatosFormulario(): void {
+    console.log(this.materia.value);
+  }
 }
+
